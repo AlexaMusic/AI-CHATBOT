6432c456833df25ebc3d3d7fee3eea52326121dd
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-02-13 04:26:48 +0500

    Ai

---
+++ Aibot.py
@@ -0,0 +1,37 @@
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Set up the chat bot
+chatbot = ChatBot('MyBot')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train('chatterbot.corpus.english')
+
+# Set up the Telegram bot
+TOKEN = 'YOUR_TOKEN_HERE'
+bot = telegram.Bot(token=TOKEN)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+# Define the start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm an AI chat bot! Ask me anything.")
+
+# Define the echo command
+def echo(update, context):
+    message = update.message.text
+    response = chatbot.get_response(message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=str(response))
+
+# Set up the Telegram bot handlers
+updater = Updater(TOKEN, use_context=True)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+# Start the bot
+updater.start_polling()
+updater.idle()
