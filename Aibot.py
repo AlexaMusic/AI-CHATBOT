3b7099ac7ad01543363d7c2b9d7a759afd7ca174
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-02-13 14:28:43 +0500

    Update Aibot.py

--- Aibot.py
+++ Aibot.py
@@ -1,36 +1,29 @@
-import logging
+import openai
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# Set up the chat bot
-chatbot = ChatBot('MyBot')
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
+# Set up the OpenAI API key
+openai.api_key = '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'
 
-# Set up the Telegram bot
-TOKEN = '5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g'
-bot = telegram.Bot(token=TOKEN)
+# Define the message handler function
+def chat(update, context):
+    # Get the user's input
+    user_input = update.message.text
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+    # Use the OpenAI API to generate a response
+    response = openai.Completion.create(
+        engine="davinci", prompt=user_input, max_tokens=1024, n=1, stop=None, temperature=0.5
+    )
+    bot_response = response.choices[0].text
 
-# Define the start command
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm an AI chat bot! Ask me anything.")
+    # Send the response back to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=bot_response)
 
-# Define the echo command
-def echo(update, context):
-    message = update.message.text
-    response = chatbot.get_response(message)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=str(response))
+# Set up the bot
+updater = Updater('5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g', use_context=True)
 
-# Set up the Telegram bot handlers
-updater = Updater(TOKEN, use_context=True)
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+# Add the message handler to the bot
+updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, chat))
 
 # Start the bot
 updater.start_polling()
