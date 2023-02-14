9e9cc4295c31b7bf210e9526cdc227d59604da6b
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-02-14 15:55:53 +0500

    Update Aibot.py

--- Aibot.py
+++ Aibot.py
@@ -1,37 +1,56 @@
 import logging
-import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# Set up the chat bot
-chatbot = ChatBot('MyBot')
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
-
-# Set up the Telegram bot
-TOKEN = '5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g'
-bot = telegram.Bot(token=TOKEN)
-
-# Enable logging
+# Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-# Define the start command
+# Define a dictionary to store secret codes and verified users
+users = {}
+
+# Define a function to handle the start command
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm an AI chat bot! Ask me anything.")
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! Please enter the secret code to verify your account.')
 
-# Define the echo command
-def echo(update, context):
-    message = update.message.text
-    response = chatbot.get_response(message)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=str(response))
+# Define a function to handle messages containing the secret code
+def verify(update, context):
+    """Verify the user's account with the secret code."""
+    code = update.message.text
+    if code in users:
+        users[update.message.from_user.id] = True
+        update.message.reply_text('Your account has been verified!')
+    else:
+        update.message.reply_text('Sorry, that code is not valid.')
 
-# Set up the Telegram bot handlers
-updater = Updater(TOKEN, use_context=True)
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+# Define a function to handle messages from unverified users
+def unverified(update, context):
+    """Send a message to unverified users."""
+    if update.message.from_user.id not in users:
+        update.message.reply_text('Sorry, you must be verified to use this bot.')
 
-# Start the bot
-updater.start_polling()
-updater.idle()
+# Define the main function to run the bot
+def main():
+    """Start the bot."""
+    # Create the Updater and pass in your bot's token
+    updater = Updater("5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add command handlers
+    dp.add_handler(CommandHandler("start", start))
+
+    # Add message handlers
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, verify))
+    dp.add_handler(MessageHandler(Filters.all, unverified))
+
+    # Start the bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
+    updater.idle()
+
+# Call the main function to start the bot
+if __name__ == '__main__':
+    main()
